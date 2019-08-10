8ead6072d64c2dcd37e7ab17c8c1eb9a586718b9
Alastair Williams <bootcamp@alwm.net>
2019-08-10 14:30:15 -0600

    Completed database model and seeding script

---
+++ db/seed.js
@@ -0,0 +1,46 @@
+const db = require('./index.js')
+const Promise = require('bluebird')
+
+const seedRandom = function (numRecords) {
+  const before = new Date()
+
+  const randomString = () => Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 5)
+  const randomNum = (max, min=0) => Math.floor(Math.random() * (max - min)) + min
+  const randomTags = () => new Array(Math.floor(randomNum(5))).fill('').map(() => randomString())
+
+  // Example product must be included, so seed n-1 random tracks plus
+  // one track with the eaxample product data.
+  const tracks = new Array(numRecords - 1).fill(null).map(() => {
+    return db.save({
+      name: randomString(),
+      artist: randomString(),
+      cdn_url: `http://www.whatever.com/${randomString()}.mp3`,
+      tags: randomTags(),
+      plays: randomNum(100000),
+      likes: randomNum(1000),
+      reposts: randomNum(100)
+    })
+  })
+
+  // Example Product for the entire team
+  tracks.push(db.save({
+      name: 'Little Bugs',
+      artist: 'AmigoKing',
+      cdn_url: `http://www.whatever.com/${randomString()}.mp3`,
+      tags: randomTags(),
+      plays: randomNum(100000),
+      likes: randomNum(1000),
+      reposts: randomNum(100)
+  }))
+
+  Promise.all(tracks).then((results) => {
+    const after = new Date();
+    console.log(`Seeded ${results.length} records in ${(after - before)/1000} seconds.`);
+  }).catch((err) => {
+    console.error('Error seeding records: ', err);
+  }).finally(() => {
+    process.exit();
+  })
+}
+
+seedRandom(100);
