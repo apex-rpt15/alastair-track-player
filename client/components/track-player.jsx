8a3608cc46a83868c758ce779316bceecfd81cba
Alastair Williams <bootcamp@alwm.net>
2019-08-27 20:54:43 -0600

    css changes to all but waveform

--- client/components/track-player.jsx
+++ client/components/track-player.jsx
@@ -13,27 +13,31 @@ export default class TrackPlayer extends Component {
         <div className="backgroundGradient" style={styles.bgGradient}></div>
           <div className="trackArea" style={styles.trackArea}>
             <span className="trackArt" style={styles.trackArt}></span>
-            <div className="titleArea">
+            <div className="titleArea" style={styles.titleArea}>
               <div className="playButtonDiv" style={styles.playButtonDiv}>
-                <button className="playLink" style={styles.playLink}>Play</button>
+                <button className="playLink" style={styles.playLink}></button>
               </div>
-              <div className="artistLabel">
-                <a href={`/${this.props.track.artist}`}>{this.props.track.artist}</a>
+              <div className="artistTrackContainer" style={styles.artistTrackContainer}>
+                <div className="artistLabelContainer" style={styles.artistLabelContainer}>
+                  <a style={styles.artistLabel} href={`/${this.props.track.artist}`}>{this.props.track.artist}</a>
                 </div>
-                <span>
+                <span style={styles.trackLabel}>
                   {this.props.track.name}
                 </span>
+              </div>
             </div>
           </div>
           <div className="trackInfo" style={styles.trackInfo}>
-            <time className="relativeTime" dateTime={this.props.track.date_posted}>
-              {moment(new Date(this.props.track.date_posted)).fromNow()}
-            </time>
-              <div>
-              {this.props.track.tags.map((tag, index) => 
-                <a key={index} href={`/tags/${tag}`} className="trackTag">{'#'+tag}</a>)}
-              </div>
+            <div className="timeContainer" style={styles.timeContainer}>
+              <time className="relativeTime" style={styles.relativeTime} dateTime={this.props.track.date_posted}>
+                {moment(new Date(this.props.track.date_posted)).fromNow()}
+              </time>
             </div>
+            <div>
+              {this.props.track.tags.map((tag, index) => 
+                <a key={index} href={`/tags/${tag}`} className="trackTag" style={styles.trackTag}>{'#'+tag}</a>)}
+            </div>
+          </div>
           <div className="waveformArea">
 
           </div>
